cf16755da988f9c3830dde63712ede6ca0e5a153
Rohit Jain <rohit.jain.324@gmail.com>
2022-06-09 20:35:46 +0200

    added loops for middle square

--- LED_Only_Projects/1_pixel_test.py
+++ LED_Only_Projects/1_pixel_test.py
@@ -66,8 +66,17 @@ def runner():
 				for y in range(1,6):
 					mySense.set_pixel(p1x, y, 255, 0, 0) 
 			mySense.set_pixel(p2x, p2y, 0, 255, 0)
+			if p2y!= 0 or p2y != 7 :
+				for x in range (1,6):
+					mySense.set_pixel(p2x, p2y, 0, 255, 0)
 			mySense.set_pixel(p3x, p3y, 0, 0, 255)
+			if p3y!= 0 or p3y != 7 :
+				for x in range (1,6):
+					mySense.set_pixel(p3x, p3y, 0, 0, 255)
 			mySense.set_pixel(p4x, p4y, 255, 0, 255)
+			if p4x != 0 or p4x != 7 :
+				for y in range(1,6):
+					mySense.set_pixel(p4x, p4y, 255, 0, 255)
 
 			# Debug Info
 			pixels = "P1(" + str(p1x) + "," + str(p1y) + ")"
